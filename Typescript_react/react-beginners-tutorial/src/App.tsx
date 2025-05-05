195c5ce4a3b51decef36fc4a07c34db5bac2aec4
shisa3110 <en5725do.0113@gmail.com>
2025-05-05 19:06:54 +0900

    家計簿アプリの入力欄作成

--- Typescript_react/react-beginners-tutorial/src/App.tsx
+++ Typescript_react/react-beginners-tutorial/src/App.tsx
@@ -1,9 +1,23 @@
-import { Text } from "@chakra-ui/react";
+import { Button, Checkbox, Flex, Text } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react"
+
 
 function App() {
   return (
     <div>
       <Text fontSize="2xl">家計簿アプリ</Text>
+      <div>
+        <Input placeholder="タイトルを入力" size="md" mb="4px"/>
+        <Input placeholder="金額を入力" size="md" mb="4px" />
+        <Flex align="center" justifyContent="space-between">
+          <Checkbox.Root>
+            <Checkbox.HiddenInput />
+            <Checkbox.Control />
+            <Checkbox.Label>入金</Checkbox.Label>
+          </Checkbox.Root>
+          <Button>追加</Button>
+        </Flex>
+      </div>
     </div>
   );
 }
