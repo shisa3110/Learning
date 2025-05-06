d46f439c41a6aeba39434f48a34ceb69be2c3173
shisa3110 <en5725do.0113@gmail.com>
2025-05-06 18:07:20 +0900

    家計簿アプリの入力データ保存・表示の設定

--- Typescript_react/react-beginners-tutorial/src/App.tsx
+++ Typescript_react/react-beginners-tutorial/src/App.tsx
@@ -1,22 +1,67 @@
 import { Button, Checkbox, Flex, Text } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/react"
+import { useState } from "react";
 
+
+type Record = {
+  id: number;
+  title: string;
+  amount: number;
+  isIncome: boolean;
+};
 
 function App() {
+  const [records, setRecords] = useState<Record[]>([]);
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [isIncome, setIsIncome] = useState(false);
+
   return (
     <div>
       <Text fontSize="2xl">家計簿アプリ</Text>
       <div>
-        <Input placeholder="タイトルを入力" size="md" mb="4px"/>
-        <Input placeholder="金額を入力" size="md" mb="4px" />
+        <Input
+        placeholder="タイトルを入力"
+        size="md" mb="4px"
+        onChange={(e) => setTitle(e.target.value)}
+        />
+        <Input
+        placeholder="金額を入力"
+        size="md" mb="4px"
+        type = "number"
+        onChange={(e) => setAmount(Number(e.target.value))}
+        />
         <Flex align="center" justifyContent="space-between">
-          <Checkbox.Root>
+          <Checkbox.Root onChange={() => setIsIncome(!isIncome)}>
             <Checkbox.HiddenInput />
             <Checkbox.Control />
             <Checkbox.Label>入金</Checkbox.Label>
           </Checkbox.Root>
-          <Button>追加</Button>
+          <Button
+            onClick={() => {
+            const newRecord: Record = {
+            id: records.length + 1,
+            title: title,
+            amount: amount,
+            isIncome,
+          };
+
+          setRecords([...records, newRecord]);
+          }}>追加</Button>
         </Flex>
+      </div>
+      <div>
+        {records.map((record) => (
+          <div key={record.id}>
+            <Flex align="center" justifyContent="space-between">
+            <Text>{record.title}</Text>
+            <Text>
+              {record.isIncome ? "+" : "-"}
+              {record.amount}
+            </Text>
+            </Flex>
+          </div>
+        ))}
       </div>
     </div>
   );
